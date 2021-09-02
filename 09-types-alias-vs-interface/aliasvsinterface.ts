69d1dab12b27d97c0cd21180a27953f36b5edeb9
Dante <layscontato@gmail.com>
2021-09-02 09:08:12 -0300

    aliasvsinterface-interface

--- 09-types-alias-vs-interface/aliasvsinterface.ts
+++ 09-types-alias-vs-interface/aliasvsinterface.ts
@@ -1,4 +1,9 @@
-//Type Alias 
+//####################### ALIAS VS INTERFACE #################################           
+
+//########################################################           
+//############################ Type Alias 
+//######################################################## 
+
 //definição
 type GameT = {
   title: string;
@@ -37,3 +42,52 @@ type JQueryT = { b: string };
 //na maioria das vezes 
 // React - Props
 // Consistência!
+
+//########################################################           
+//############################ Type Interface
+//######################################################## 
+
+//definição
+interface Game {
+  title: string;
+}
+
+interface DLC {
+  extra: string;
+}
+
+// intersecção | extend
+interface GameCollection extends Game, DLC {}
+
+//implements
+class CreateGame implements GameCollection {}
+
+//declarar função
+interface getSimilars {
+  (title: string): void;
+}
+// ################# diferenças 
+interface ID extends number {}
+
+interface Tuple {
+  0: number;
+  1: number;
+}
+
+[1,2,3,4, "sass"] as Tuple;
+
+//Pode ter múltiplas declarações e ele une de mesmo nome
+interface JQuery {
+  a: string;
+}
+interface JQuery {
+  b: string;
+}
+
+const $: JQuery = {
+  a: "bla",
+  b: "foo",
+}
+
+//vantagegem: quando tiver criando libs, prefira Interface, porque são mais extensíveis
+//cirnado objeto/classes (POO)
