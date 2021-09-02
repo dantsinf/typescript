badd182cd9c74ee686daafdc82c7eaa86641230f
Dante <layscontato@gmail.com>
2021-09-02 08:56:48 -0300

    aliasvsinterface-alias final

--- 09-types-alias-vs-interface/aliasvsinterface.ts
+++ 09-types-alias-vs-interface/aliasvsinterface.ts
@@ -18,3 +18,22 @@ class CreateGameT implements GameCollection {}
 type getSimilarsT = (title: string) => void;
 
 // ################# diferenças 
+
+//permite declarar tipos primitivos
+type IDT = string | number;
+
+//permite declarar tuplas normalmente
+type TupleT = [number, number];
+
+//isso da erro pois acima foi declarado 2x o number
+//[1,2,3] as TupleT;
+[1,2] as TupleT;
+
+//Apenas uma declaração por escopo
+type JQueryT = { a: string };
+type JQueryT = { b: string };
+
+//mais remomentadado:
+//na maioria das vezes 
+// React - Props
+// Consistência!
