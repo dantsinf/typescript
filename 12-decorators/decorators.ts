e1f7b8274f6eee901ed4ce07695d1a9b0f80a0cb
Dante <layscontato@gmail.com>
2021-09-02 15:28:08 -0300

    decorators, ainda editando

--- 12-decorators/decorators.ts
+++ 12-decorators/decorators.ts
@@ -9,12 +9,42 @@ Method decorator
 Parameter decorator
 Acessor decorator
 */
+//decorator é uma função que recebe parametros por default e retorna
 
 //Factory
+/*
 function Logger(prefix: string){
   return (target) => {
-    console.log(target);
+    console.log(`${prefix} - ${target}`);
   };
 }
 @Logger("awesome")
 class Foo {}
+*/
+
+//############ Class Decorator
+function setAPIVersion(apiVersion: string) {
+  return (constructor) => {
+    return class extends constructor {
+      version = apiVersion
+    }
+  }
+}
+
+// decorator - anotar a versão da API
+//@setAPIVersion("1.0.0")
+//class API {}
+
+//console.log(new API());
+
+//Property decorator
+
+class Movie {
+  //validação - se for menor que 5 - error
+  title: string;
+
+  constructor(t: string) {
+    this.title = t;
+  }
+
+}
