63f26a8352bcccd27c1b42cab7ea1e52e688762d
Dante <layscontato@gmail.com>
2021-09-05 22:15:37 -0300

    calculadora basica, readme atualizdo

--- projetos/calc/calc/src/App.js
+++ projetos/calc/calc/src/App.js
@@ -7,6 +7,14 @@ const App = () => {
     setResultado(resultado.concat(e.target.value));
   };
 
+  const limpa = () => {
+    setResultado('');
+  };
+
+  const espaco = () => {
+    setResultado(resultado.slice(0, -1));
+  };
+
   return (
     <>
       <div className="container">
@@ -15,9 +23,58 @@ const App = () => {
         </form>
 
         <div className="botoes">
-          <button onClick={handleClick}>Limpar</button>
-          <button onClick={handleClick}>C</button>
-          <button onClick={handleClick}>&divide;</button>
+          <button onClick={limpa} id="limpa">
+            Limpar
+          </button>
+          <button onClick={espaco} id="espaco">
+            C
+          </button>
+          <button name="/" onClick={handleClick}>
+            &divide;
+          </button>
+          <button name="7" onClick={handleClick}>
+            7
+          </button>
+          <button name="8" onClick={handleClick}>
+            8
+          </button>
+          <button name="9" onClick={handleClick}>
+            9
+          </button>
+          <button name="*" onClick={handleClick}>
+            &times;
+          </button>
+          <button name="4" onClick={handleClick}>
+            4
+          </button>
+          <button name="5" onClick={handleClick}>
+            5
+          </button>
+          <button name="6" onClick={handleClick}>
+            6
+          </button>
+          <button name="-" onClick={handleClick}>
+            &ndash;
+          </button>
+          <button name="1" onClick={handleClick}>
+            1
+          </button>
+          <button name="2" onClick={handleClick}>
+            2
+          </button>
+          <button name="3" onClick={handleClick}>
+            3
+          </button>
+          <button name="+" onClick={handleClick}>
+            +
+          </button>
+          <button name="0" onClick={handleClick}>
+            0
+          </button>
+          <button name="*" onClick={handleClick}>
+            .
+          </button>
+          <button onClick={handleClick}>=</button>
         </div>
       </div>
     </>
