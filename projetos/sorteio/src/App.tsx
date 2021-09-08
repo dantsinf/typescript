983d35ff644fb363729ee20a9af175e28e2b2563
Dante <layscontato@gmail.com>
2021-09-08 14:47:27 -0300

    renderizacao e estilo

--- projetos/sorteio/src/App.tsx
+++ projetos/sorteio/src/App.tsx
@@ -6,16 +6,19 @@ import { useState } from "react";
 function App() {
 
   const [cont, setCont] = useState<number[]>([]);
-  const [max, setMax] = useState<number | string>();
+  //const [max, setMax] = useState<number | string>();
+  const mostra = cont.map((e, i) => (
+    <p key={i}>{e}</p>
+  ));
+
 
   return (
     <div className="App">
       {/*<button onClick={()=> setCont([...cont, cont.length+1])}>Add array</button>*/}
       {/*<button onClick={()=> setCont([...cont, Math.random()])}>Add array</button>*/}
-      <input type="number" placeholder="Escreva o valor máximo" value={max} onChange={(m) => setMax(m.target.value)}/>
-      <button onClick={()=> setCont([...cont, Math.floor(Math.random()*10)])}>Add array</button>
-      {JSON.stringify(cont)}
-      <p>Max: {max}</p>
+      {/*<input type="number" placeholder="Escreva o valor máximo" value={max} onChange={(m) => setMax(m.target.value)}/>*/}
+      <button onClick={()=> setCont([...cont, Math.floor(Math.random()* 7)+1])}>Sortear</button>
+        <p>{mostra}</p>
     </div>
   );
 }
