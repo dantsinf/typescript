1e35086b91da2da8fc9d154e4c73648715825401
Dante <layscontato@gmail.com>
2021-09-09 12:02:20 -0300

    add handlechange

--- projetos/ficha-medica/src/AddToListas.tsx
+++ projetos/ficha-medica/src/AddToListas.tsx
@@ -1,13 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const AddToListas = () => {
+
+  const [input, setInput] =  useState({
+    nome: "",
+    idade: "",
+    descricao: "",
+    img: ""
+  })
+
+  const handleChange = () => {
+
+  }
+
   return (
     <div className="AddToList" >
       <p>Lista de adicionar</p>
-      <input type="text" placeholder="Nome" className="AddToList-input" />
-      <input type="text" placeholder="Idade" className="AddToList-input" />
-      <input type="text" placeholder="Imagem Url" className="AddToList-input" />
-      <input type="text" placeholder="Descrição" className="AddToList-input" />
+      <input type="text" placeholder="Nome" value={input.nome} onClick={handleChange} className="AddToList-input" name="nome" />
+      <input type="text" placeholder="Idade" value={input.idade} onClick={handleChange} className="AddToList-input" name="idade" />
+      <input type="text" placeholder="Imagem Url" value={input.img} onClick={handleChange} className="AddToList-input" name="img" />
+      <textarea placeholder="Descrição" value={input.descricao} onClick={handleChange} className="AddToList-input" name="descricao" />
     </div>
   )
 }
