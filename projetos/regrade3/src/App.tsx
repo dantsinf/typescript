dd4004df5b077088bbbe489bf6b49efb64f69df9
Dante <39998160+dantsinf@users.noreply.github.com>
2021-09-14 18:16:45 -0300

    app atualizado

--- projetos/regrade3/src/App.tsx
+++ projetos/regrade3/src/App.tsx
@@ -2,14 +2,60 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  /*
+  a - b
+c - x
+
+
+(c*b)/a
+  */
+
+  const [a, setA] = React.useState();
+  const [b, setB] = React.useState();
+  const [c, setC] = React.useState();
+
+  const Calcular = () => {
+    return (c * b) / a;
+  };
+
+  /*const Resposta = ({ Calcular }) => {
+    if (Calcular() == null) {
+      <p>É nulo</p>;
+    }
+  };*/
+
   return (
-    <div className="App">
-      <input type="text" placeholder="A" />
-      <input type="text" placeholder="B" />
-      <br />
-      <input type="text" placeholder="C" />
-      <input type="text" placeholder="X" />
-    </div>
+    <>
+      <div className="App">
+        <div className="primeiradiv">
+          <input
+            type="text"
+            placeholder="A"
+            nome="ia"
+            value={a}
+            onChange={(inpa) => setA(inpa.target.value)}
+          />
+          <span>Está para </span>
+          <input
+            type="text"
+            placeholder="B"
+            value={b}
+            onChange={(b) => setB(b.target.value)}
+          />
+        </div>
+        <br />
+        <div className="segundadiv">
+          <input
+            type="text"
+            placeholder="C"
+            value={c}
+            onChange={(c) => setC(c.target.value)}
+          />
+          <span>Está para </span>
+          <input type="text" placeholder="X" value={Calcular()} />
+        </div>
+      </div>
+    </>
   );
 }
 
