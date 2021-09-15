d2fd93462d8cdb8db5b8bd240308f4575852e5db
Dante <layscontato@gmail.com>
2021-09-15 11:42:52 -0300

    ajustando erros

--- projetos/sorteio/src/App.tsx
+++ projetos/sorteio/src/App.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import './App.css';
-import { useState } from "react";
 
+/*type Ordem = {
+  
+}*/
 
 function App() {
-
-  const [cont, setCont] = useState<number[]>([]);
-  //const [max, setMax] = useState<number | string>();
+  const [cont, setCont] = React.useState<number[]>([]);
   const mostra = cont.map((e, i) => (
     <p key={i}>{e}</p>
   ));
 
 
   return (
+    <>
     <div className="App">
       {/*<button onClick={()=> setCont([...cont, cont.length+1])}>Add array</button>*/}
       {/*<button onClick={()=> setCont([...cont, Math.random()])}>Add array</button>*/}
@@ -20,6 +21,7 @@ function App() {
       <button onClick={()=> setCont([...cont, Math.floor(Math.random()* 7)+1])}>Sortear</button>
         <p>{mostra}</p>
     </div>
+    </>
   );
 }
 
