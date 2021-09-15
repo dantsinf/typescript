7c3664ac4336a8ec17db1eeb0a3cf5fe8f1fbbca
Dante <layscontato@gmail.com>
2021-09-15 12:05:13 -0300

    add input

--- projetos/sorteio/src/App.tsx
+++ projetos/sorteio/src/App.tsx
@@ -7,10 +7,17 @@ import './App.css';
 
 function App() {
   const [cont, setCont] = React.useState<number[]>([]);
-  const mostra = cont.map((e, i) => (
+  const [min, setMin] = React.useState<number>();
+  const [max, setMax] = React.useState<number>();
+  /*const mostra = cont.map((e, i) => (
     <p key={i}>{e}</p>
-  ));
-
+  ));*/
+  
+  const Resultado = (e: number, i: number) => {
+    return cont.map((e, i) => (
+      <p key={i}>{e}</p>
+    ))
+  }
 
   return (
     <>
@@ -18,8 +25,13 @@ function App() {
       {/*<button onClick={()=> setCont([...cont, cont.length+1])}>Add array</button>*/}
       {/*<button onClick={()=> setCont([...cont, Math.random()])}>Add array</button>*/}
       {/*<input type="number" placeholder="Escreva o valor máximo" value={max} onChange={(m) => setMax(m.target.value)}/>*/}
+      {/*onChange={(a) => setA(parseInt(a.target.value))}*/}
+      <input type="text" name="min" placeholder="Min" onChange={(i)=> setMin(parseInt(i.target.value))}/>
+      <input type="text" name="max" placeholder="Max" onChange={(a)=> setMax(parseInt(a.target.value))}/>
       <button onClick={()=> setCont([...cont, Math.floor(Math.random()* 7)+1])}>Sortear</button>
-        <p>{mostra}</p>
+        <p>{Resultado(0,0)}</p>
+        <p>{min}</p>
+        <p>{max}</p>
     </div>
     </>
   );
