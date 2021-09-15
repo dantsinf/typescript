9ba8cf9428132f000089c49208a65ba324c442be
Dante Rosa Ulguim <dante_ulguim@sicredi.com.br>
2021-09-15 16:59:28 -0300

    esilizado e add botao +

--- projetos/calc/calc/src/App.js
+++ projetos/calc/calc/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./App.css";
 /*eslint-disable no-eval */
 
 const App = () => {
@@ -6,9 +7,34 @@ const App = () => {
   const [n1, setN1] = React.useState();
   const [n2, setN2] = React.useState();
 
+  /*const Calcular = () => {
+    return setResultado(val.target.value);
+  };*/
+
   return (
     <>
-      <div className="container"></div>
+      <div className="container">
+        <input
+          type="text"
+          name="n1"
+          onChange={np => setN1(parseInt(np.target.value))}
+        />
+        <input
+          type="text"
+          name="n2"
+          onChange={ns => setN2(parseInt(ns.target.value))}
+        />
+        <button
+          value="+"
+          name="+"
+          onClick={val => setResultado(val.target.value)}
+        >
+          +
+        </button>
+        <p>{n1}</p>
+        <p>{n2}</p>
+        <p>{resultado}</p>
+      </div>
     </>
   );
 };
