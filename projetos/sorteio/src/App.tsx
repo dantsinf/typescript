061bb2f1aee5f1ed472cdb0a59de217e4f9356ea
Dante Rosa Ulguim <dante_ulguim@sicredi.com.br>
2021-09-15 13:57:01 -0300

    estilizado

--- projetos/sorteio/src/App.tsx
+++ projetos/sorteio/src/App.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 /*type Ordem = {
   
@@ -7,32 +7,50 @@ import './App.css';
 
 function App() {
   const [cont, setCont] = React.useState<number[]>([]);
-  const [min, setMin] = React.useState<number>();
-  const [max, setMax] = React.useState<number>();
+  const [min, setMin] = React.useState<number>(0);
+  const [max, setMax] = React.useState<number>(0);
   /*const mostra = cont.map((e, i) => (
     <p key={i}>{e}</p>
   ));*/
-  
+
   const Resultado = (e: number, i: number) => {
-    return cont.map((e, i) => (
-      <p key={i}>{e}</p>
-    ))
-  }
+    return cont.map((e, i) => <p key={i}>{e}</p>);
+  };
 
   return (
     <>
-    <div className="App">
-      {/*<button onClick={()=> setCont([...cont, cont.length+1])}>Add array</button>*/}
-      {/*<button onClick={()=> setCont([...cont, Math.random()])}>Add array</button>*/}
-      {/*<input type="number" placeholder="Escreva o valor máximo" value={max} onChange={(m) => setMax(m.target.value)}/>*/}
-      {/*onChange={(a) => setA(parseInt(a.target.value))}*/}
-      <input type="text" name="min" placeholder="Min" onChange={(i)=> setMin(parseInt(i.target.value))}/>
-      <input type="text" name="max" placeholder="Max" onChange={(a)=> setMax(parseInt(a.target.value))}/>
-      <button onClick={()=> setCont([...cont, Math.floor(Math.random()* 7)+1])}>Sortear</button>
-        <p>{Resultado(0,0)}</p>
-        <p>{min}</p>
-        <p>{max}</p>
-    </div>
+      <div className="App">
+        {/*<button onClick={()=> setCont([...cont, cont.length+1])}>Add array</button>*/}
+        {/*<button onClick={()=> setCont([...cont, Math.random()])}>Add array</button>*/}
+        {/*<input type="number" placeholder="Escreva o valor máximo" value={max} onChange={(m) => setMax(m.target.value)}/>*/}
+        {/*onChange={(a) => setA(parseInt(a.target.value))}*/}
+        <div className="campo">
+          <span>Entre</span>
+          <input
+            className="campo-numero"
+            type="text"
+            name="min"
+            placeholder="Min"
+            onChange={i => setMin(parseInt(i.target.value))}
+          />
+          <span>e</span>
+          <input
+            className="campo-numero"
+            type="text"
+            name="max"
+            placeholder="Max"
+            onChange={a => setMax(parseInt(a.target.value))}
+          />
+          <button
+            onClick={() =>
+              setCont([...cont, Math.floor(Math.random() * max) + min])
+            }
+          >
+            Sortear
+          </button>
+          <p>{Resultado(0, 0)}</p>
+        </div>
+      </div>
     </>
   );
 }
