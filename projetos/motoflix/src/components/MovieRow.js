2eba577fb36ce65901c8f424cd2b9c68cef03737
Dante <layscontato@gmail.com>
2021-09-21 16:37:13 -0300

    alterando MovieRow

--- projetos/motoflix/src/components/MovieRow.js
+++ projetos/motoflix/src/components/MovieRow.js
@@ -4,6 +4,11 @@ const MovieRow = ({ title, items }) => {
   return (
     <div>
       <h2>{title}</h2>
+      <div className="movieRow--listarea">
+        {items.results.lenght > 0 && items.results.map((item, key)=>(
+
+        ))}
+      </div>
     </div>
   );
 };
