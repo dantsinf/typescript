fb288133489817a70f9b4294ecc56a1d5c991685
Dante <layscontato@gmail.com>
2021-09-22 10:28:44 -0300

    arrumando erro no img

--- projetos/motoflix/src/components/MovieRow.js
+++ projetos/motoflix/src/components/MovieRow.js
@@ -5,7 +5,7 @@ const MovieRow = ({ title, items }) => {
     <div>
       <h2>{title}</h2>
       <div className="movieRow--listarea">
-        {items.results.lenght > 0 &&
+        {items.results.length > 0 &&
           items.results.map((item, key) => (
             <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} />
           ))}
