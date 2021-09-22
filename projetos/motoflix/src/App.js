788fec8a88dacd920b7f70795d9116a2f75e0a47
Dante <layscontato@gmail.com>
2021-09-22 15:32:54 -0300

    useState, filter

--- projetos/motoflix/src/App.js
+++ projetos/motoflix/src/App.js
@@ -2,22 +2,26 @@ import React, { useEffect, useState } from 'react';
 import Tmdb from './Tmdb';
 import './App.css';
 import MovieRow from './components/MovieRow';
+import FeaturedMovie from './components/FeaturedMovie';
 
 const App = () => {
   const [movieList, setMovieList] = useState([]);
-
+  const [featuredData, setfeaturedData] = useState(null);
   useEffect(() => {
     const loadAll = async () => {
       //Pegando a lista total
       let list = await Tmdb.getHomeList();
       setMovieList(list);
-      //console.log(list);
+      //Pegando o Featured
+      let originals = list.filter((i) => i.slug === 'originals');
     };
     loadAll();
   }, []);
 
   return (
     <div className="page">
+      {featuredData && <FeaturedMovie item={featuredData} />}
+
       <section className="lists">
         {movieList.map((item, key) => (
           <MovieRow key={key} title={item.title} items={item.items} />
