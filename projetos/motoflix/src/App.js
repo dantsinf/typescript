dde99efa7eea2cfeaff3b2f98479643e7820b2f7
Dante <layscontato@gmail.com>
2021-09-24 10:33:26 -0300

    bateria de teste, erro items

--- projetos/motoflix/src/App.js
+++ projetos/motoflix/src/App.js
@@ -1,24 +1,26 @@
 import React, { useEffect, useState } from 'react';
-import Tmdb from './Tmdb';
 import './App.css';
+import Tmdb from './Tmdb';
 import MovieRow from './components/MovieRow';
 import FeaturedMovie from './components/FeaturedMovie';
 
 const App = () => {
   const [movieList, setMovieList] = useState([]);
-  const [featuredData, setfeaturedData] = useState(null);
+  const [featuredData, setFeaturedData] = useState(null);
   useEffect(() => {
     const loadAll = async () => {
       //Pegando a lista total
       let list = await Tmdb.getHomeList();
       setMovieList(list);
+
+      /*
       //Pegando o Featured
       let originals = list.filter((i) => i.slug === 'originals');
       let randomChosen = Math.floor(
-        Math.random() * (originals[0].items.results.length - 1),
+      Math.random() * (originals[0].items.results.length - 1),
       );
       let chosen = originals[0].items.results[randomChosen];
-      console.log(chosen);
+      console.log(chosen);*/
     };
     loadAll();
   }, []);
