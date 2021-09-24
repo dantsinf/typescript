26d437ebe1c548c6e1f9c22c82989a38760e6198
Dante <layscontato@gmail.com>
2021-09-24 16:41:25 -0300

    erro em items

--- projetos/motoflix/src/App.js
+++ projetos/motoflix/src/App.js
@@ -13,14 +13,13 @@ const App = () => {
       let list = await Tmdb.getHomeList();
       setMovieList(list);
 
-      /*
       //Pegando o Featured
       let originals = list.filter((i) => i.slug === 'originals');
       let randomChosen = Math.floor(
-      Math.random() * (originals[0].items.results.length - 1),
+        Math.random() * originals[0].items.results.length - 1,
       );
       let chosen = originals[0].items.results[randomChosen];
-      console.log(chosen);*/
+      console.log(chosen);
     };
     loadAll();
   }, []);
