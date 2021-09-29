1d679e3872e9a9e60b1af22971f3f83791a9aea8
Dante <layscontato@gmail.com>
2021-09-29 12:01:20 -0300

    async, objeto state

--- projetos/dadosapi/src/App.js
+++ projetos/dadosapi/src/App.js
@@ -1,6 +1,18 @@
 import './App.css';
+import React from 'react';
+import api from './api';
 
 function App() {
+  const state = {
+    filmes: [],
+  };
+
+  async function componentDidMout() {
+    const response = await api.get('');
+    console.log(response.data);
+    this.setState({ filmes: response.data });
+  }
+  const { filmes } = state;
   return (
     <div className="App">
       <h1>Listar artigos</h1>
