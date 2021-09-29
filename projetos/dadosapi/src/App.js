91134b5d26968246db1e7068f4805c32fb9f852b
Dante <layscontato@gmail.com>
2021-09-29 13:30:01 -0300

    add map e console.log

--- projetos/dadosapi/src/App.js
+++ projetos/dadosapi/src/App.js
@@ -9,13 +9,22 @@ function App() {
 
   async function componentDidMout() {
     const response = await api.get('');
-    console.log(response.data);
+    // console.log(response.data);
     this.setState({ filmes: response.data });
   }
   const { filmes } = state;
   return (
     <div className="App">
       <h1>Listar artigos</h1>
+      {console.log(filmes)}
+      {filmes.map((filme) => (
+        <li key={filme.show.id}>
+          <h2>
+            {' '}
+            <strong>Título</strong>{' '}
+          </h2>
+        </li>
+      ))}
     </div>
   );
 }
