2f5ae2e28176fd139804efecca84ad8bf459b271
Dante <layscontato@gmail.com>
2021-10-18 13:47:21 -0300

    types, Item.ts

--- projetos/aula-bonieky-lacerda/aula2/src/data/items.ts
+++ projetos/aula-bonieky-lacerda/aula2/src/data/items.ts
@@ -1,3 +1,8 @@
-  export const items = [
-    {date: new Date(2021, 9, 12), category: 'food', title:'McDonalds', value: 35.15}
+  import { Item } from '../types/Item'
+  
+  export const items: Item[] = [
+    {date: new Date(2021, 9, 12), category: 'food', title:'McDonalds', value: 35.15},
+    {date: new Date(2021, 9, 12), category: 'food', title:'Burger King', value: 28},
+    {date: new Date(2021, 9, 16), category: 'rent', title:'Alugue Apt', value: 2300},
+    {date: new Date(2021, 10, 18), category: 'salary', title:'Salário ACME', value: 4500},
   ];
