1306bbaaab3dd90c5522e9da333242fbfae3220b
Dante <layscontato@gmail.com>
2021-10-18 11:10:17 -0300

    App, App.styles.ts

--- projetos/aula-bonieky-lacerda/aula2/src/App.tsx
+++ projetos/aula-bonieky-lacerda/aula2/src/App.tsx
@@ -1,13 +1,19 @@
- import React from 'react';
-import './App.css';
+import React from 'react'
+import * as C from './App.styles';
 
-function App() {
-
+const App = () => {
   return (
-    <div className="App">  
-
+    <div>
+      <C.Container>
+        <C.Header>
+          <C.HeaderText>Sistema Finaceiro</C.HeaderText>
+        </C.Header>
+        <C.Body>
+          ...
+        </C.Body>
+      </C.Container>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
