2394785bbf85b9c975cb22fa07761376623887dd
Dante Rosa Ulguim <dante_ulguim@sicredi.com.br>
2021-10-20 16:07:13 -0300

    dateFilter

--- projetos/aula-bonieky-lacerda/aula2/src/helpers/dateFilter.ts
+++ projetos/aula-bonieky-lacerda/aula2/src/helpers/dateFilter.ts
@@ -1,40 +1,62 @@
-import { Item } from '../types/Item';
+import { Item } from "../types/Item";
 
 //filtros de datas e edição delas
 export const getCurrentMonth = () => {
   let now = new Date();
-  return `${now.getFullYear()}-${now.getMonth()+1}`;
-}
+  return `${now.getFullYear()}-${now.getMonth() + 1}`;
+};
 
 //o retorno é logo após : e é o array de Item. ex.: : Item[] => {
 export const filterListByMonth = (list: Item[], date: string): Item[] => {
   let newList: Item[] = [];
-  let [year, month] = date.split('-');
-  for (let i in list){
-    if(
+  let [year, month] = date.split("-");
+  for (let i in list) {
+    if (
       list[i].date.getFullYear() === parseInt(year) &&
-      (list[i].date.getMonth() + 1) === parseInt(month)
-    ){
-        newList.push(list[i]);
+      list[i].date.getMonth() + 1 === parseInt(month)
+    ) {
+      newList.push(list[i]);
     }
   }
 
   return newList;
-}
+};
 
 export const formatDate = (date: Date): string => {
   let year = date.getFullYear();
-  let  month = date.getMonth()+1;
+  let month = date.getMonth() + 1;
   let day = date.getDate();
 
   return `${addZeroToDate(day)}/${addZeroToDate(month)}/${year}`;
-}
+};
 
-{/* adicionando zero na frente do ida e mês */}
+{
+  /* adicionando zero na frente do ida e mês */
+}
 const addZeroToDate = (n: number): string => {
-  if(n<10){
+  if (n < 10) {
     return `0${n}`;
-  }else{
+  } else {
     return `${n}`;
   }
-}
+};
+
+export const FormatCurrentMonth = (currentMonth: string): string => {
+  let [year, month] = currentMonth.split("-");
+  let months = [
+    "Janeiro",
+    "Fevereiro",
+    "Março",
+    "Abril",
+    "Maio",
+    "Junho",
+    "Julho",
+    "Agosto",
+    "Setembro",
+    "Outubro",
+    "Novembro",
+    "Dezembro",
+  ];
+
+  return `${months[parseInt(month) - 1]} ${year}`;
+};
