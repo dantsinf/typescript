46ee271114fdbd513d5da9d4d976d11341a0400f
Dante <layscontato@gmail.com>
2021-10-22 14:37:52 -0300

    App.styles, Spp

--- projetos/aula-bonieky-lacerda/aula4-galeria/src/libs/firebase.ts
+++ projetos/aula-bonieky-lacerda/aula4-galeria/src/libs/firebase.ts
@@ -5,13 +5,12 @@ import { getStorage } from 'firebase/storage';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAEl4oZVwU2wjIh0rxDVsZ7_ZGZ6iWdgqs",
-  authDomain: "d5react.firebaseapp.com",
-  projectId: "d5react",
-  storageBucket: "d5react.appspot.com",
-  messagingSenderId: "287585518136",
-  appId: "1:287585518136:web:8d3f8597cbaa6721245ccd",
-  measurementId: "G-ZKL2EFLPBZ"
+  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSEAGINGSENDERID,
+  appId: process.env.REACT_APP_FIREBASE_APID
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
