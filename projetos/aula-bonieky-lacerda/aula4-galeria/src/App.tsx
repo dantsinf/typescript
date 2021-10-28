427d42bb2f6878f118827a2378e41a08166b513c
Dante <layscontato@gmail.com>
2021-10-28 11:44:30 -0300

    App, Photos

--- projetos/aula-bonieky-lacerda/aula4-galeria/src/App.tsx
+++ projetos/aula-bonieky-lacerda/aula4-galeria/src/App.tsx
@@ -2,11 +2,23 @@ import { useState, useEffect } from 'react';
 import './App.css';
 import * as C from './App.styles';
 import * as Photos from './services/photos';
+import {Photo} from './types/Photos'
 
 
 function App() {
 
   const [loading, setLoading] = useState(false);
+  const [photos, setPhotos] = useState<Photo[]>([]);
+
+  useEffect(()=>{
+    const getPhotos = async()=>{
+      setLoading(true);
+      //let photos = await Photos.getAll();
+      setPhotos(await Photos.getAll())
+      setLoading(false);
+    }
+    getPhotos();
+  },[])
 
   return (
     <div className="App">
